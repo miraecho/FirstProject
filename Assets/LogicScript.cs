ebe2812b418cc2aed95d0b62f24a5527b4b52a22
miraecho <49175337+miraecho@users.noreply.github.com>
2026-07-08 22:06:02 -0700

    Added Player Animation

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -49,3 +50,39 @@ public class LogicScript : MonoBehaviour
     }
 
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LogicScript : MonoBehaviour
+{
+    public int playerScore;
+    public int highScore;
+    public Text scoreText;
+    public Text highScoreText;
+    public GameObject gameOverScreen;
+
+    [ContextMenu("Increase Score")]
+    public void addScore(int scoreToAdd)
+    {
+        playerScore = playerScore + scoreToAdd;
+        scoreText.text = playerScore.ToString();
+        highScore += playerScore;
+        highScoreText.text = highScore.ToString();
+    }
+
+    public void restartGame() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver() 
+    {
+        gameOverScreen.SetActive(true);
+    }
+
+}
+>>>>>>> Stashed changes
